0587302be1206e3c25f7ada956cf9a44f1d65efb
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-08-10 22:33:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
- if num % 3 == 0 
+  if num % 3 == 0 
    "Fizz"
- end
- if num % 5 == 0 
+  else if num % 5 == 0 
    "Buzz"
- end
+  else if num % 5 == 0 && num % 3 == 0 
+    "FizzBuzz"
+  end
 end
